fa65e0dbc5cefdbd3555a409d7b7683334bb0861
Renaud Gaubert <rgaubert@nvidia.com>
2017-12-29 17:01:32 +0100

    Sleep if this a non GPU node
    Fixes #17

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ func main() {
 	log.Println("Loading NVML")
 	if err := nvml.Init(); err != nil {
 		log.Printf("Failed to start nvml with error: %s.", err)
-		log.Printf("Did you set the docker default runtime to `nvidia`?")
+		log.Printf("If this is a GPU node, did you set the docker default runtime to `nvidia`?")
 		log.Printf("You can check the prerequisites at: https://github.com/NVIDIA/k8s-device-plugin#prerequisites")
 		log.Printf("You can learn how to set the runtime at: https://github.com/NVIDIA/k8s-device-plugin#quick-start")
 
-		os.Exit(1)
+		select {}
 	}
 	defer func() { log.Println("Shutdown of NVML returned:", nvml.Shutdown()) }()
 
