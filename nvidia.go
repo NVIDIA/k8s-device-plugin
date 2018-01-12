4451a6aa7239ab044a130295de1f4971b1e823eb
Renaud Gaubert <rgaubert@nvidia.com>
2018-01-13 00:44:16 +0100

    Mark older GPUs as unhealthy if we can't healthcheck

--- nvidia.go
+++ nvidia.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/NVIDIA/nvidia-docker/src/nvml"
 
@@ -47,8 +48,19 @@ func watchXIDs(ctx context.Context, devs []*pluginapi.Device, xids chan<- *plugi
 	eventSet := nvml.NewEventSet()
 	defer nvml.DeleteEventSet(eventSet)
 
-	err := nvml.RegisterEvent(eventSet, nvml.XidCriticalError)
-	check(err)
+	for _, d := range devs {
+		err := nvml.RegisterEventForDevice(eventSet, nvml.XidCriticalError, d.ID)
+		if err != nil && strings.HasSuffix(err.Error(), "Not Supported") {
+			log.Printf("Warning: GPU with UUID %s is too old to support healtchecking with error: %s. Marking it unhealthy.", d.ID)
+
+			xids <- d
+			continue
+		}
+
+		if err != nil {
+			log.Panicln("Fatal:", err)
+		}
+	}
 
 	for {
 		select {
