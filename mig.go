297cb7171bdd35ea860befdc8068c0c0eddd8b12
Evan Lezar <elezar@nvidia.com>
2021-02-08 12:05:10 +0100

    Format error message when no MIG devices are found
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- mig.go
+++ mig.go
@@ -81,7 +81,7 @@ func (devices *MIGCapableDevices) AssertAllMigEnabledDevicesAreValid() error {
 			return err
 		}
 		if len(migs) == 0 {
-			return fmt.Errorf("No MIG devices associated with %v", d)
+			return fmt.Errorf("No MIG devices associated with %v: %v", d.Path, d.UUID)
 		}
 	}
 	return nil
