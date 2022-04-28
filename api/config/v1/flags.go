0f97edfa7a068488acb8d329f13007b0c8fcaa14
Kevin Klues <kklues@nvidia.com>
2022-04-28 08:20:48 +0000

    Update config file to be used across plugin and GFD
    In general, its a little weird to have the combined config live in just one of
    the two repos, but since these components are so tightly coupled, it's not too
    terrible. In the future we should consider merging the plugin and GFD into the
    same code base, but continue to maintain separate binaries and helm charts /
    daemonset yamls. It probably makes the most sense if/when we move to calendar
    versioning instead of semantic versioning for these components.
    
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- api/config/v1/flags.go
+++ api/config/v1/flags.go
@@ -17,44 +17,75 @@
 package v1
 
 import (
+	"time"
+
 	cli "github.com/urfave/cli/v2"
 )
 
-// Flags holds the full list of flags used to configure the device plugin.
+// Flags holds the full list of flags used to configure the device plugin and GFD.
 type Flags struct {
 	*CommandLineFlags
 }
 
-// CommandLineFlags holds the list of command line flags used to configure the device plugin.
+// CommandLineFlags holds the list of command line flags used to configure the device plugin and GFD.
 type CommandLineFlags struct {
-	MigStrategy        string `json:"migStrategy"        yaml:"migStrategy"`
-	FailOnInitError    bool   `json:"failOnInitError"    yaml:"failOnInitError"`
+	MigStrategy      string                 `json:"migStrategy"      yaml:"migStrategy"`
+	FailOnInitError  bool                   `json:"failOnInitError"  yaml:"failOnInitError"`
+	NvidiaDriverRoot string                 `json:"nvidiaDriverRoot" yaml:"nvidiaDriverRoot"`
+	Plugin           PluginCommandLineFlags `json:"plugin"           yaml:"plugin"`
+	GFD              GFDCommandLineFlags    `json:"gfd"              yaml:"gfd"`
+}
+
+// PluginCommandLineFlags holds the list of command line flags specific to the device plugin.
+type PluginCommandLineFlags struct {
 	PassDeviceSpecs    bool   `json:"passDeviceSpecs"    yaml:"passDeviceSpecs"`
 	DeviceListStrategy string `json:"deviceListStrategy" yaml:"deviceListStrategy"`
 	DeviceIDStrategy   string `json:"deviceIDStrategy"   yaml:"deviceIDStrategy"`
-	NvidiaDriverRoot   string `json:"nvidiaDriverRoot"   yaml:"nvidiaDriverRoot"`
+}
+
+// GFDCommandLineFlags holds the list of command line flags specific to GFD.
+type GFDCommandLineFlags struct {
+	Oneshot       bool          `json:"oneshot"       yaml:"oneshot"`
+	NoTimestamp   bool          `json:"noTimestamp"   yaml:"noTimestamp"`
+	SleepInterval time.Duration `json:"sleepInterval" yaml:"sleepInterval"`
+	OutputFile    string        `json:"outputFile"    yaml:"outputFile"`
 }
 
 // NewCommandLineFlags builds out a CommandLineFlags struct from the flags in cli.Context.
 func NewCommandLineFlags(c *cli.Context) *CommandLineFlags {
 	return &CommandLineFlags{
-		MigStrategy:        c.String("mig-strategy"),
-		FailOnInitError:    c.Bool("fail-on-init-error"),
-		PassDeviceSpecs:    c.Bool("pass-device-specs"),
-		DeviceListStrategy: c.String("device-list-strategy"),
-		DeviceIDStrategy:   c.String("device-id-strategy"),
-		NvidiaDriverRoot:   c.String("nvidia-driver-root"),
+		MigStrategy:      c.String("mig-strategy"),
+		FailOnInitError:  c.Bool("fail-on-init-error"),
+		NvidiaDriverRoot: c.String("nvidia-driver-root"),
+		Plugin: PluginCommandLineFlags{
+			PassDeviceSpecs:    c.Bool("pass-device-specs"),
+			DeviceListStrategy: c.String("device-list-strategy"),
+			DeviceIDStrategy:   c.String("device-id-strategy"),
+		},
+		GFD: GFDCommandLineFlags{
+			Oneshot:       c.Bool("oneshot"),
+			OutputFile:    c.String("output-file"),
+			SleepInterval: c.Duration("sleep-interval"),
+			NoTimestamp:   c.Bool("no-timestamp"),
+		},
 	}
 }
 
 // ToMap converts a Flags struct into a generic map[interface{}]interface{}
 func (f *Flags) ToMap() map[interface{}]interface{} {
 	return map[interface{}]interface{}{
-		"mig-strategy":         f.MigStrategy,
-		"fail-on-init-error":   f.FailOnInitError,
-		"pass-device-specs":    f.PassDeviceSpecs,
-		"device-list-strategy": f.DeviceListStrategy,
-		"device-id-strategy":   f.DeviceIDStrategy,
-		"nvidia-driver-root":   f.NvidiaDriverRoot,
+		// Common flags
+		"mig-strategy":       f.MigStrategy,
+		"fail-on-init-error": f.FailOnInitError,
+		"nvidia-driver-root": f.NvidiaDriverRoot,
+		// Plugin specific flags
+		"pass-device-specs":    f.Plugin.PassDeviceSpecs,
+		"device-list-strategy": f.Plugin.DeviceListStrategy,
+		"device-id-strategy":   f.Plugin.DeviceIDStrategy,
+		// GFD specific flags
+		"oneshot":        f.GFD.Oneshot,
+		"output-file":    f.GFD.OutputFile,
+		"sleep-interval": f.GFD.SleepInterval,
+		"no-timestamp":   f.GFD.NoTimestamp,
 	}
 }
