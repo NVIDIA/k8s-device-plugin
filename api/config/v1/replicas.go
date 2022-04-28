a7c5dcf6091495da2b83d4dd5a6125b620f04d3f
Kevin Klues <kklues@nvidia.com>
2022-04-28 20:20:32 +0000

    Add API for specifying time-slicing parameters in the config file
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

---
+++ api/config/v1/replicas.go
@@ -0,0 +1,304 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/google/uuid"
+	k8s "k8s.io/apimachinery/pkg/api/validation"
+)
+
+// These constants define the possible strategies for striping through GPUs when time-slicing them.
+const (
+	UnspecifiedTimeSlicingStrategy = ""
+	// TODO: Add support for additional strategies below
+	//RoundRobinTimeSlicingStrategy = "round-robin"
+	//PackedTimeSlicingStrategy     = "packed"
+)
+
+// TimeSlicing defines the set of replicas to be made for time-slicing available resources.
+type TimeSlicing struct {
+	Strategy  string                     `json:"strategy,omitempty" yaml:"strategy,omitempty"`
+	Resources map[string]ReplicaResource `json:"resources"          yaml:"resources"`
+}
+
+// ReplicaResource represents a resource to be replicated.
+type ReplicaResource struct {
+	Devices  ReplicaDevices `json:"devices"          yaml:"devices,flow"`
+	Replicas int            `json:"replicas"         yaml:"replicas"`
+	Rename   string         `json:"rename,omitempty" yaml:"rename,omitempty"`
+}
+
+// ReplicaDevices encapsulates the set of devices that should be replicated for a given resource.
+// This struct should be treated as a 'union' and only one of the fields in this struct should be set at any given time.
+type ReplicaDevices struct {
+	All   bool
+	Count int
+	List  []ReplicaDeviceRef
+}
+
+// ReplicaDeviceRef can either be a full GPU index, a MIG index, or a UUID (full GPU or MIG)
+type ReplicaDeviceRef string
+
+// IsGPUIndex checks if a ReplicaDeviceRef is a full GPU index
+func (d ReplicaDeviceRef) IsGPUIndex() bool {
+	if _, err := strconv.ParseUint(string(d), 10, 0); err != nil {
+		return false
+	}
+	return true
+}
+
+// IsMigIndex checks if a ReplicaDeviceRef is a MIG index
+func (d ReplicaDeviceRef) IsMigIndex() bool {
+	split := strings.SplitN(string(d), ":", 2)
+	if len(split) != 2 {
+		return false
+	}
+	for _, s := range split {
+		if _, err := strconv.ParseUint(s, 10, 0); err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+// IsUUID checks if a ReplicaDeviceRef is a UUID
+func (d ReplicaDeviceRef) IsUUID() bool {
+	return d.IsGpuUUID() || d.IsMigUUID()
+}
+
+// IsGpuUUID checks if a ReplicaDeviceRef is a GPU UUID
+// A GPU UUID must be of the form GPU-b1028956-cfa2-0990-bf4a-5da9abb51763
+func (d ReplicaDeviceRef) IsGpuUUID() bool {
+	if !strings.HasPrefix(string(d), "GPU-") {
+		return false
+	}
+	_, err := uuid.Parse(strings.TrimPrefix(string(d), "GPU-"))
+	return err == nil
+}
+
+// IsMigUUID checks if a ReplicaDeviceRef is a MIG UUID
+// A MIG UUID can be of one of two forms:
+//    - MIG-b1028956-cfa2-0990-bf4a-5da9abb51763
+//    - MIG-GPU-b1028956-cfa2-0990-bf4a-5da9abb51763/3/0
+func (d ReplicaDeviceRef) IsMigUUID() bool {
+	if !strings.HasPrefix(string(d), "MIG-") {
+		return false
+	}
+	suffix := strings.TrimPrefix(string(d), "MIG-")
+	_, err := uuid.Parse(suffix)
+	if err == nil {
+		return true
+	}
+	split := strings.SplitN(suffix, "/", 3)
+	if len(split) != 3 {
+		return false
+	}
+	if !ReplicaDeviceRef(split[0]).IsGpuUUID() {
+		return false
+	}
+	for _, s := range split[1:] {
+		_, err := strconv.ParseUint(s, 10, 0)
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+// UnmarshalJSON unmarshals raw bytes into a 'TimeSlicing' struct.
+func (s *TimeSlicing) UnmarshalJSON(b []byte) error {
+	ts := make(map[string]json.RawMessage)
+	err := json.Unmarshal(b, &ts)
+	if err != nil {
+		return err
+	}
+
+	strategy, exists := ts["strategy"]
+	if !exists {
+		strategy = []byte(fmt.Sprintf(`"%s"`, UnspecifiedTimeSlicingStrategy))
+	}
+
+	err = json.Unmarshal(strategy, &s.Strategy)
+	if err != nil {
+		return err
+	}
+
+	switch s.Strategy {
+	case UnspecifiedTimeSlicingStrategy:
+	default:
+		return fmt.Errorf("unknown time-slicing strategy: %v", s.Strategy)
+	}
+
+	resources, exists := ts["resources"]
+	if !exists {
+		return fmt.Errorf("no resources specified")
+	}
+
+	rawResources := make(map[string]json.RawMessage)
+	err = json.Unmarshal(resources, &rawResources)
+	if err != nil {
+		return err
+	}
+
+	if len(rawResources) == 0 {
+		return fmt.Errorf("no resources specified")
+	}
+
+	s.Resources = make(map[string]ReplicaResource)
+	for name, resource := range rawResources {
+		invalid := k8s.NameIsDNSSubdomain(name, false)
+		if len(invalid) != 0 {
+			return fmt.Errorf("incorrect format for time-sliced resource name '%v'", name)
+		}
+
+		var rr ReplicaResource
+		err = json.Unmarshal(resource, &rr)
+		if err != nil {
+			return err
+		}
+
+		s.Resources[name] = rr
+	}
+
+	return nil
+}
+
+// UnmarshalJSON unmarshals raw bytes into a 'ReplicaResource' struct.
+func (s *ReplicaResource) UnmarshalJSON(b []byte) error {
+	rr := make(map[string]json.RawMessage)
+	err := json.Unmarshal(b, &rr)
+	if err != nil {
+		return err
+	}
+
+	devices, exists := rr["devices"]
+	if !exists {
+		devices = []byte(`"all"`)
+	}
+
+	err = json.Unmarshal(devices, &s.Devices)
+	if err != nil {
+		return err
+	}
+
+	replicas, exists := rr["replicas"]
+	if !exists {
+		return fmt.Errorf("no replicas specified")
+	}
+
+	err = json.Unmarshal(replicas, &s.Replicas)
+	if err != nil {
+		return err
+	}
+
+	if s.Replicas <= 0 {
+		return fmt.Errorf("number of replicas must be >= 0")
+	}
+
+	rename, exists := rr["rename"]
+	if !exists {
+		return nil
+	}
+
+	err = json.Unmarshal(rename, &s.Rename)
+	if err != nil {
+		return err
+	}
+
+	invalid := k8s.NameIsDNSSubdomain(s.Rename, false)
+	if len(invalid) != 0 {
+		return fmt.Errorf("incorrect format for renamed resource '%v'", s.Rename)
+	}
+
+	return nil
+}
+
+// UnmarshalJSON unmarshals raw bytes into a 'ReplicaDevices' struct.
+func (s *ReplicaDevices) UnmarshalJSON(b []byte) error {
+	// Match the string 'all'
+	var str string
+	err := json.Unmarshal(b, &str)
+	if err == nil {
+		if str != "all" {
+			return fmt.Errorf("devices set as '%v' but the only valid string input is 'all'", str)
+		}
+		s.All = true
+		return nil
+	}
+
+	// Match a count
+	var count int
+	err = json.Unmarshal(b, &count)
+	if err == nil {
+		if count <= 0 {
+			return fmt.Errorf("devices set as '%v' but a count of devices must be > 0", count)
+		}
+		s.Count = count
+		return nil
+	}
+
+	// Match a list
+	var slice []json.RawMessage
+	err = json.Unmarshal(b, &slice)
+	if err == nil {
+		// For each item in the list check its format and convert it to a string (if necessary)
+		result := make([]ReplicaDeviceRef, len(slice))
+		for i, s := range slice {
+			// Match a uint as a GPU index and convert it to a string
+			var index uint
+			if err = json.Unmarshal(s, &index); err == nil {
+				result[i] = ReplicaDeviceRef(strconv.Itoa(int(index)))
+				continue
+			}
+			// Match strings as valid entries if they are GPU indices, MIG indices, or UUIDs
+			var item string
+			if err = json.Unmarshal(s, &item); err == nil {
+				rd := ReplicaDeviceRef(item)
+				if rd.IsGPUIndex() || rd.IsMigIndex() || rd.IsUUID() {
+					result[i] = rd
+					continue
+				}
+			}
+			// Treat any other entries as errors
+			return fmt.Errorf("unsupported type for device in devices list: %v, %T", item, item)
+		}
+		s.List = result
+		return nil
+	}
+
+	// No matches found
+	return fmt.Errorf("unrecognized type for devices spec: %v", string(b))
+}
+
+// MarshalJSON marshals ReplicaDevices to its raw bytes representation
+func (s *ReplicaDevices) MarshalJSON() ([]byte, error) {
+	if s.All == true {
+		return json.Marshal("all")
+	}
+	if s.Count > 0 {
+		return json.Marshal(s.Count)
+	}
+	if s.List != nil {
+		return json.Marshal(s.List)
+	}
+	return nil, fmt.Errorf("unmarshallable ReplicaDevices struct: %v", s)
+}
---
+++ api/config/v1/replicas_test.go
@@ -0,0 +1,466 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReplicaDeviceRef(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "0",
+			expected: "gpuIndex",
+		},
+		{
+			input:    "0:0",
+			expected: "migIndex",
+		},
+		{
+			input:    "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c",
+			expected: "uuid",
+		},
+		{
+			input:    "MIG-3eb87630-93d5-b2b6-b8ff-9b359caf4ee2",
+			expected: "uuid",
+		},
+		{
+			input:    "MIG-GPU-662077db-fa3f-0d8f-9502-21ab0ef058a2/10/0",
+			expected: "uuid",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			switch tc.expected {
+			case "gpuIndex":
+				require.True(t, ReplicaDeviceRef(tc.input).IsGPUIndex())
+				require.False(t, ReplicaDeviceRef(tc.input).IsMigIndex())
+				require.False(t, ReplicaDeviceRef(tc.input).IsUUID())
+			case "migIndex":
+				require.False(t, ReplicaDeviceRef(tc.input).IsGPUIndex())
+				require.True(t, ReplicaDeviceRef(tc.input).IsMigIndex())
+				require.False(t, ReplicaDeviceRef(tc.input).IsUUID())
+			case "uuid":
+				require.False(t, ReplicaDeviceRef(tc.input).IsGPUIndex())
+				require.False(t, ReplicaDeviceRef(tc.input).IsMigIndex())
+				require.True(t, ReplicaDeviceRef(tc.input).IsUUID())
+			}
+		})
+	}
+}
+
+func TestMarshalReplicaDevices(t *testing.T) {
+	testCases := []struct {
+		input  ReplicaDevices
+		output string
+		err    bool
+	}{
+		{
+			input: ReplicaDevices{},
+			err:   true,
+		},
+		{
+			input: ReplicaDevices{
+				All: true,
+			},
+			output: `"all"`,
+		},
+		{
+			input: ReplicaDevices{
+				Count: 2,
+			},
+			output: `2`,
+		},
+		{
+			input: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0", "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"},
+			},
+			output: `["0", "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			output, err := tc.input.MarshalJSON()
+			if tc.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.JSONEq(t, tc.output, string(output))
+		})
+	}
+}
+
+func TestUnmarshalReplicaDevices(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output ReplicaDevices
+		err    bool
+	}{
+		{
+			input: ``,
+			err:   true,
+		},
+		{
+			input: `"not-all"`,
+			err:   true,
+		},
+		{
+			input: `-2`,
+			err:   true,
+		},
+		{
+			input: `2.0`,
+			err:   true,
+		},
+		{
+			input: `[-1]`,
+			err:   true,
+		},
+		{
+			input: `["-1"]`,
+			err:   true,
+		},
+		{
+			input: `["invalid-UUID"]`,
+			err:   true,
+		},
+		{
+			input: `["GPU-UUID"]`,
+			err:   true,
+		},
+		{
+			input: `["MIG-UUID"]`,
+			err:   true,
+		},
+		{
+			input: `["MIG-GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+			err:   true,
+		},
+		{
+			input: `"all"`,
+			output: ReplicaDevices{
+				All: true,
+			},
+		},
+		{
+			input: `2`,
+			output: ReplicaDevices{
+				Count: 2,
+			},
+		},
+		{
+			input: `[0]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0"},
+			},
+		},
+		{
+			input: `["0"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0"},
+			},
+		},
+		{
+			input: `["0:0"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0:0"},
+			},
+		},
+		{
+			input: `["GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"},
+			},
+		},
+		{
+			input: `["MIG-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"MIG-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"},
+			},
+		},
+		{
+			input: `["MIG-GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c/0/0"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"MIG-GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c/0/0"},
+			},
+		},
+		{
+			input: `[0, "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0", "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"},
+			},
+		},
+		{
+			input: `["0", "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"]`,
+			output: ReplicaDevices{
+				List: []ReplicaDeviceRef{"0", "0:0", "GPU-4cf8db2d-06c0-7d70-1a51-e59b25b2c16c"},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			var output ReplicaDevices
+			err := output.UnmarshalJSON([]byte(tc.input))
+			if tc.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.output, output)
+		})
+	}
+}
+
+func TestUnmarshalReplicaResource(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output ReplicaResource
+		err    bool
+	}{
+		{
+			input: ``,
+			err:   true,
+		},
+		{
+			input: `{}`,
+			err:   true,
+		},
+		{
+			input: `{
+				"devices": "all",
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"devices": "all",
+				"rename": "valid",
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"devices": "all",
+				"replicas": 2
+			}`,
+			output: ReplicaResource{
+				Devices:  ReplicaDevices{All: true},
+				Replicas: 2,
+			},
+		},
+		{
+			input: `{
+				"devices": "all",
+				"replicas": 2,
+				"rename": "valid"
+			}`,
+			output: ReplicaResource{
+				Devices:  ReplicaDevices{All: true},
+				Replicas: 2,
+				Rename:   "valid",
+			},
+		},
+		{
+			input: `{
+				"replicas": -1,
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"replicas": 0,
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"replicas": 2
+			}`,
+			output: ReplicaResource{
+				Devices:  ReplicaDevices{All: true},
+				Replicas: 2,
+			},
+		},
+		{
+			input: `{
+				"replicas": 2,
+				"rename": "valid"
+			}`,
+			output: ReplicaResource{
+				Devices:  ReplicaDevices{All: true},
+				Replicas: 2,
+				Rename:   "valid",
+			},
+		},
+		{
+			input: `{
+				"replicas": 2,
+				"rename": "$invalid$"
+			}`,
+			err: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			var output ReplicaResource
+			err := output.UnmarshalJSON([]byte(tc.input))
+			if tc.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.output, output)
+		})
+	}
+}
+
+func TestUnmarshalTimeSlicing(t *testing.T) {
+	testCases := []struct {
+		input  string
+		output TimeSlicing
+		err    bool
+	}{
+		{
+			input: ``,
+			err:   true,
+		},
+		{
+			input: `{}`,
+			err:   true,
+		},
+		{
+			input: `{
+				"strategy,": "",
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"strategy": "",
+				"resources": {}
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"strategy": "",
+				"resources": {
+					"valid": {
+						"replicas": 2
+					}
+				}
+			}`,
+			output: TimeSlicing{
+				Strategy: UnspecifiedTimeSlicingStrategy,
+				Resources: map[string]ReplicaResource{
+					"valid": {
+						Devices:  ReplicaDevices{All: true},
+						Replicas: 2,
+					},
+				},
+			},
+		},
+		{
+			input: `{
+				"resources": {
+					"valid": {
+						"replicas": 2
+					}
+				}
+			}`,
+			output: TimeSlicing{
+				Strategy: UnspecifiedTimeSlicingStrategy,
+				Resources: map[string]ReplicaResource{
+					"valid": {
+						Devices:  ReplicaDevices{All: true},
+						Replicas: 2,
+					},
+				},
+			},
+		},
+		{
+			input: `{
+				"resources": {
+					"valid1": {
+						"replicas": 2
+					},
+					"valid2": {
+						"replicas": 2
+					}
+				}
+			}`,
+			output: TimeSlicing{
+				Strategy: UnspecifiedTimeSlicingStrategy,
+				Resources: map[string]ReplicaResource{
+					"valid1": {
+						Devices:  ReplicaDevices{All: true},
+						Replicas: 2,
+					},
+					"valid2": {
+						Devices:  ReplicaDevices{All: true},
+						Replicas: 2,
+					},
+				},
+			},
+		},
+		{
+			input: `{
+				"strategy": "bogus",
+				"resources": {
+					"valid": {
+						"replicas": 2
+					}
+				}
+			}`,
+			err: true,
+		},
+		{
+			input: `{
+				"resources": {
+					"$invalid$": {
+						"replicas": 2
+					}
+				}
+			}`,
+			err: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			var output TimeSlicing
+			err := output.UnmarshalJSON([]byte(tc.input))
+			if tc.err {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.output, output)
+		})
+	}
+}
