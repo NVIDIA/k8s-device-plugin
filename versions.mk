69c31c20eb3d6bf727197d3fd4acf0a2eaa69a78
Evan Lezar <7723350-elezar@users.noreply.gitlab.com>
2023-03-28 21:06:55 +0000

    Merge branch 'bump-version-0.14.0-rc.3' into 'release-0.14'
    Bump version to v0.14.0-rc.3
    
    See merge request nvidia/kubernetes/device-plugin!282

--- versions.mk
+++ versions.mk
@@ -14,7 +14,7 @@
 
 MODULE := github.com/NVIDIA/k8s-device-plugin
 
-VERSION  ?= v0.14.0-rc.2
+VERSION  ?= v0.14.0-rc.3
 
 # vVERSION represents the version with a guaranteed v-prefix
 vVERSION := v$(VERSION:v%=%)
