7d3f6db3c278712b266a864e6142b0a1d773358a
Evan Lezar <elezar@nvidia.com>
2023-06-07 17:42:07 +0200

    Update go version to 1.20.5
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -20,6 +20,6 @@ VERSION  ?= v0.14.0
 vVERSION := v$(VERSION:v%=%)
 
 CUDA_VERSION ?= 12.1.1
-GOLANG_VERSION ?= 1.20.2
+GOLANG_VERSION ?= 1.20.5
 
 GIT_COMMIT ?= $(shell git describe --match="" --dirty --long --always --abbrev=40 2> /dev/null || echo "")
