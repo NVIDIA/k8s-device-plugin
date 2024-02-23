28b7d3af7061bf06de732d1748ec964870811df0
Evan Lezar <elezar@nvidia.com>
2024-02-23 15:20:23 +0200

    Bump binary version and chart version to v0.15.0-rc.1
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -17,7 +17,7 @@ MODULE := github.com/NVIDIA/$(DRIVER_NAME)
 
 REGISTRY ?= nvcr.io/nvidia
 
-VERSION  ?= v0.14.4
+VERSION  ?= v0.15.0-rc.1
 
 # vVERSION represents the version with a guaranteed v-prefix
 vVERSION := v$(VERSION:v%=%)
