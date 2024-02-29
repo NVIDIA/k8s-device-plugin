0c2975b705ba815dbf5c581685ee5aa3ff5e831a
Evan Lezar <elezar@nvidia.com>
2024-02-29 10:55:31 +0200

    Bump version to v0.14.5
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -14,7 +14,7 @@
 
 MODULE := github.com/NVIDIA/k8s-device-plugin
 
-VERSION  ?= v0.14.4
+VERSION  ?= v0.14.5
 
 # vVERSION represents the version with a guaranteed v-prefix
 vVERSION := v$(VERSION:v%=%)
