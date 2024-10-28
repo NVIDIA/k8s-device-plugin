e7bd77543526627232b6c62ba7c34b93de48bff8
Evan Lezar <elezar@nvidia.com>
2024-10-28 14:57:33 +0100

    Bump version for v0.17.0-rc.1 release
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -17,7 +17,7 @@ MODULE := github.com/NVIDIA/$(DRIVER_NAME)
 
 REGISTRY ?= nvcr.io/nvidia
 
-VERSION ?= v0.16.2
+VERSION ?= v0.17.0-rc.1
 
 # vVERSION represents the version with a guaranteed v-prefix
 vVERSION := v$(VERSION:v%=%)
