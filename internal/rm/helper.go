2b572473798c1c005975018d8d874e773634b6e7
Tariq Ibrahim <tibrahim@nvidia.com>
2026-06-09 18:17:47 -0700

    update internal.rm package to handle schema change in PciInfo
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- internal/rm/helper.go
+++ internal/rm/helper.go
@@ -16,17 +16,19 @@
 
 package rm
 
-// uint8Slice wraps an []uint8 with more functions.
-type uint8Slice []uint8
-
-// String turns a nil terminated uint8Slice into a string
-func (s uint8Slice) String() string {
+func nullTerminatedIntSliceToString[T int8 | uint8](s []T) string {
 	var b []byte
 	for _, c := range s {
 		if c == 0 {
 			break
 		}
-		b = append(b, c)
+		b = append(b, byte(c))
 	}
 	return string(b)
 }
+
+// int8Slice wraps an []int8 with more functions.
+type int8Slice []int8
+
+// String turns a nil terminated int8Slice into a string.
+func (s int8Slice) String() string { return nullTerminatedIntSliceToString(s) }
